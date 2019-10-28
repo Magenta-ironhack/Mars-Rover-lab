0c1cb024f9934d6ce8877f8d98b1591f8ab3a222
Pradeepa Shanmugasundaram <pradeepa@Pradeepas-MacBook-Pro.local>
2019-10-28 16:30:33 -0400

    added obs

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+class Obstacle {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+}
 class Rover {
-    constructor(direction) {
-this.direction = direction;
+    constructor(name) {
+        this.name = name;
+this.direction = 'N';
 this.x = 0;
 this.y = 0;
 this.travellog = [];
@@ -27,6 +34,7 @@ this.travellog = [];
     }
     move(dir) {
          // *Directions are "f" (forward), "b" (backward).
+        
     if (dir === "f") {
         switch (this.direction) {
           case "N":
@@ -100,9 +108,18 @@ this.travellog = [];
             }
             break;
         }
-        console.log(
-          `Rover is at (x${this.x},y${this.y}) ::: Travel Log: ${this.travellog}`
-        );
+        obstacleArray.forEach(obstacle => {
+            console.log(this.x,obstacle.x);
+            if(obstacle.x === this.x && obstacle.y === this.y) {
+                console.log('Caution - Obstacle ' + obstacle +' is in the path');
+                this.travellog.pop();
+            } else {
+                console.log(
+                    `Rover is at (x${this.x},y${this.y}) ::: Travel Log: ${this.travellog}`
+                  );
+            }
+        })
+        
       } 
     }
     location(dir) {
@@ -120,121 +137,13 @@ this.travellog = [];
           }
     }
 }
-
-const roverApollo13 = new Rover('S');
-roverApollo13.path('fffbb');
-// let rover = {
-//   direction: "N", // *Directions are "N" (North), "S" (South), "E" (East), "W" (West).
-//   x: 0,
-//   y: 0,
-//   travellog: [],
-//   turn: function(dir) {
-//     if (dir === "r" || dir === "l") {
-//       // *Directions are "r" (Right), "l" (left).
-//       switch (this.direction) {
-//         case "N":
-//           dir == "r" ? (this.direction = "E") : (this.direction = "W");
-//           break;
-//         case "S":
-//           dir == "r" ? (this.direction = "W") : (this.direction = "E");
-//           break;
-//         case "E":
-//           dir == "r" ? (this.direction = "S") : (this.direction = "N");
-//           break;
-//         case "W":
-//           dir == "r" ? (this.direction = "N") : (this.direction = "S");
-//           break;
-//       }
-//       console.log(`${dir} was called, rover is heading ${this.direction}.`);
-//     } // else { throw Error(`Invalid turning direction '${dir}'. Rover has stopped.`); }
-//   },
-//   move: function(dir) {
-//     // *Directions are "f" (forward), "b" (backward).
-//     if (dir === "f") {
-//       switch (this.direction) {
-//         case "N":
-//           if (this.y === 0) {
-//             throw Error(`Rover is at y${this.y}, can't move North.`);
-//           } else {
-//             this.y--;
-//             this.travellog.push(`(x${this.x},y${this.y})`);
-//           }
-//           break;
-//         case "S":
-//           if (this.y === 9) {
-//             throw Error(`Rover is at y${this.y}, can't move South.`);
-//           } else {
-//             this.y++;
-//             this.travellog.push(`(x${this.x},y${this.y})`);
-//           }
-//           break;
-//         case "W":
-//           if (this.x === 0) {
-//             throw Error(`Rover is at x${this.x}, can't move West.`);
-//           } else {
-//             this.x--;
-//             this.travellog.push(`(x${this.x},y${this.y})`);
-//           }
-//           break;
-//         case "E":
-//           if (this.x === 9) {
-//             throw Error(`Rover is at x${this.x}, can't move East.`);
-//           } else {
-//             this.x++;
-//             this.travellog.push(`(x${this.x},y${this.y})`);
-//           }
-//           break;
-//       }
-//       console.log(
-//         `Rover is at (x${this.x},y${this.y}) ::: Travel Log: ${this.travellog}`
-//       );
-//     } else if (dir === "b") {
-//       switch (this.direction) {
-//         case "N":
-//           if (this.y === 9) {
-//             throw Error(`Rover is at y${this.y}, can't move South.`);
-//           } else {
-//             this.y++;
-//             this.travellog.push(`(x${this.x},y${this.y})`);
-//           }
-//           break;
-//         case "S":
-//           if (this.y === 0) {
-//             throw Error(`Rover is at y${this.y}, can't move North.`);
-//           } else {
-//             this.y--;
-//             this.travellog.push(`(x${this.x},y${this.y})`);
-//           }
-//           break;
-//         case "W":
-//           if (this.x === 9) {
-//             throw Error(`Rover is at x${this.x}, can't move East.`);
-//           } else {
-//             this.x++;
-//             this.travellog.push(`(x${this.x},y${this.y})`);
-//           }
-//           break;
-//         case "E":
-//           if (this.x === 0) {
-//             throw Error(`Rover is at x${this.x}, can't move Wast.`);
-//           } else {
-//             this.x--;
-//             this.travellog.push(`(x${this.x},y${this.y})`);
-//           }
-//           break;
-//       }
-//       console.log(
-//         `Rover is at (x${this.x},y${this.y}) ::: Travel Log: ${this.travellog}`
-//       );
-//     } // else { throw Error(`Invalid moving direction '${dir}'. Rover has stopped.`)}
-//   },
-//   location: function() {
-//     console.log(
-//       `Rover is located at (x${this.x},y${this.y}), facing ${this.direction}.`
-//     );
-//   },
-// };
-
+const roverApollo13 = new Rover();
+const roverFalcon = new Rover();
+const roverChandhrayan = new Rover();
+const rock = new Obstacle(1,0);
+const disc = new Obstacle(2,3);
+const obstacleArray = [rock,disc];
+// const 
 // Welcome message:
 console.log("%c --- Rover available commands: ---", "color: orange");
 console.log(
@@ -242,12 +151,3 @@ console.log(
   "color: orange"
 );
 
-// const path = str => {
-//   if (str !== "" && str !== undefined) {
-//     str
-//       .split("")
-//       .map(char => ("fb".includes(char) ? rover.move(char) : rover.turn(char)));
-//   } else {
-//     throw Error(`No path provided.`);
-//   }
-// };
